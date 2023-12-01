f7b317c032a91b9024d4899cb9c4402558fee3b4
94CJG <krsoi9412@gmail.com>
2023-12-01 15:39:59 +0900

    ㅁ 무한스크롤 기능 작동함 -> 주영님 피드백으로 인해 구현됨

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import '../styles/Main.scss';
 import { changDate } from "../utils/ChangeInfo";
-import { useApiQuery,  useInfinityScrollApiQuery} from "../services/Api";
+import { useInfinityScrollApiQuery } from "../services/Api";
 import { useInView } from 'react-intersection-observer';
 
 interface IApi {
@@ -15,74 +15,68 @@ interface IApi {
 		original: string;
 	}
 	pub_date: string;
+	response: any
 }
 
 
 function Main() {
-	const { data:apiData, error } = useApiQuery();
-	const { data:infiniteScrollData, fetchNextPage} = useInfinityScrollApiQuery() ;
-
-	const { ref, inView} = useInView( {threshold: 0.4} );
+	const { data:infiniteScrollData, fetchNextPage } = useInfinityScrollApiQuery();
+	const { ref, inView } = useInView({ threshold: 0.4 });
 	//구조 분해를 사용 하여 data의 값들을 각각 변경하여 넣어준다.
 	//inView : boolean 값으로 해당 요소가 보이면 true를 반환하여 ref요소에 전달하고 다음을 보여준다.
 	//threshold는 관찰자의 콜백이 실행되어야 하는 대상의 가시성 백분율을 정희 한 것. 0.4 = 40%가 보이면 inView 값이 true로 설정된다.
-	console.log(apiData, error);
+	// console.log(apiData);
 	console.log(infiniteScrollData);
 
 	useEffect(() => {
-		if(inView) {
+		if (inView) {
 			fetchNextPage();
 		}
 	}, [inView])
-	
+
 	const onClick = (itemUrl: any) => {
 		return window.open(itemUrl.web_url)
 	}
 
 	return (
 		<div className="main">
-			{error ? (
-				<div>Error Test</div>
-			) : !apiData ? (
+		{!infiniteScrollData ? (
 				<div>Loading...</div>
 			) : (
 				<>
-					<div className="main-inner">
-						<ul id="card-box">
-							<div>
-								{apiData.response.docs.map((item: IApi, index: number) => (
-									<div key={index} id="card-items">
-										<li ref={ref}>
-											<div className='item-top' >
-												<div className='item-hdline'>
-													<p onClick={() => onClick(item)}>
-														{item.headline.main}
-													</p>
-												</div>
-												<div className='item-star-img'>
-													<img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABEAAAAQCAYAAADwMZRfAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAGJSURBVHgBnZNLcoJAEIaHxz7xBuQEstJyFT1BcgOTraUWN1BPkImvckc8AeUJJDtghScQTxD2vPI3NVgYC0zSVQM93T3fdDcNYzUyGAx0WuyGqLVOVbWyLAug9uri5CrHaDR6AYBJkqQNh8Mu+08mODxJkmQqy7JEOkw2+4tQFljHYj8ej4912ajUOEVRdKR+hxt1WtC1NE2NIgj7GewWwD62gejTATHBer32ZWoeDCYcunBylNFbrVbbArJYLD7wegaIU4y4zMTlVp4Jau6BuIfhNJ/PZ1UpA/Qp1B3Km9KlURTlX01xHCfsdDo7GN/a7XbDdV2b1YgA9Amw2WyCHEKPMqjVakme5zm/BZCc54RzTg2jch6rsoC/idKnZcAFREgTgT6rlgA9vPoNfg6bjiBOCs2FGDIN4NflcmlDP2A93YTgwAPmYS90DtAJYBO2ADb67FeZSIViGMY95uMLakiHMT/v6FNY+JHZBDD6nzSMQ6Psu4DEcdzH4W05oCyI0XBRVwzfWb4BTE/gjBSToccAAAAASUVORK5CYII=" alt="스크랩 별 이미지" />
-												</div>
-											</div>
-											<div className='item-bot'>
-												<div className='item-box'>
-													<div className='item-source'>
-														{item.source}
-													</div>
-													<div className='item-byline'>
-														{item.byline.original}
-													</div>
-												</div>
-												<p className='item-date'>
-													{changDate(item.pub_date)}
-												</p>
-											</div>
-										</li>
-										<br />
-									</div>
-								))}
-							</div>
-						</ul>
-					</div>
+      <div className="main-inner">
+        <ul id="card-box">
+          <div>
+            {infiniteScrollData?.pages?.map((pages: IApi[]) => {
+              return pages.map((item: IApi, i: number) => (
+                <div key={i} id="card-items" ref={ref}>
+                  <li>
+                    <div className="item-top">
+                      <div className="item-hdline">
+                        <p onClick={() => onClick(item)}>{item.headline.main}</p>
+                      </div>
+                      <div className="item-star-img">
+                        <img
+                          src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABEAAAAQCAYAAADwMZRfAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAGJSURBVHgBnZNLcoJAEIaHxz7xBuQEstJyFT1BcgOTraUWN1BPkImvckc8AeUJJDtghScQTxD2vPI3NVgYC0zSVQM93T3fdDcNYzUyGAx0WuyGqLVOVbWyLAug9uri5CrHaDR6AYBJkqQNh8Mu+08mODxJkmQqy7JEOkw2+4tQFljHYj8ej4912ajUOEVRdKR+hxt1WtC1NE2NIgj7GewWwD62gejTATHBer32ZWoeDCYcunBylNFbrVbbArJYLD7wegaIU4y4zMTlVp4Jau6BuIfhNJ/PZ1UpA/Qp1B3Km9KlURTlX01xHCfsdDo7GN/a7XbDdV2b1YgA9Amw2WyCHEKPMqjVakme5zm/BZCc54RzTg2jch6rsoC/idKnZcAFREgTgT6rlgA9vPoNfg6bjiBOCs2FGDIN4NflcmlDP2A93YTgwAPmYS90DtAJYBO2ADb67FeZSIViGMY95uMLakiHMT/v6FNY+JHZBDD6nzSMQ6Psu4DEcdzH4W05oCyI0XBRVwzfWb4BTE/gjBSToccAAAAASUVORK5CYII="
+                          alt="스크랩 별 이미지"
+                        />
+                      </div>
+                    </div>
+                    <div className="item-bot">
+                      <div className="item-box">
+                        <div className="item-source">{item.source}</div>
+                        <div className="item-byline">{item.byline.original}</div>
+                      </div>
+                      <p className="item-date">{changDate(item.pub_date)}</p>
+                    </div>
+                  </li>
+                  <br />
+                </div>
+              ));
+            })}
+          </div>
+        </ul>
+      </div>
 					<div className="main-bottom-btn">
 						<div className='btn-inner'>
 							<div className='btn-home'>
@@ -101,10 +95,3 @@ function Main() {
 	)
 }
 export default Main
-
-/**
- *질문
- * 콜백함수
- * 클로저?
- * interface를 객체로 만들어서 사용하는가? 
- */
--- src/services/Api.tsx
+++ src/services/Api.tsx
@@ -1,52 +1,63 @@
-import { useInfiniteQuery, useQuery, } from "@tanstack/react-query";
-import { useEffect } from "react";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
-export const useApiQuery = () => {
-	const { data: apiData, error, refetch } = useQuery({
-		queryKey: ['repoData'],
-		queryFn: () =>
-			fetch('https://api.nytimes.com/svc/search/v2/articlesearch.json?' +
-				'q=new+york+times&page=10' +
-				'&sort=oldest' +
-				'&pub_date' +
-				'&api-key=h6IwBnNIMxY1EfOVVAeeN0XAFFfKSfQM').then(
-				(res) => res.json(),
-			),
-	});
+// export const useApiQuery = () => {
+// 	const { data: apiData, error, refetch } = useQuery({
+// 		queryKey: ['repoData'],
+// 		queryFn: () =>
+// 			fetch('https://api.nytimes.com/svc/search/v2/articlesearch.json?' +
+// 				'q=new+york+times&page=10' +
+// 				'&sort=oldest' +
+// 				'&pub_date' +
+// 				'&api-key=h6IwBnNIMxY1EfOVVAeeN0XAFFfKSfQM').then(
+// 				(res) => res.json(),
+// 			),
+// 	});
 
-	useEffect(() => {
-		const handleActivateQuery = () => {
-			refetch();
-		};
-		handleActivateQuery();
-	}, []);
-	return { data: apiData, error };
+// 	useEffect(() => {
+// 		const handleActivateQuery = () => {
+// 			refetch();
+// 		};
+// 		handleActivateQuery();
+// 	}, []);
+// 	return { data: apiData, error };
+// }
+
+/**
+ * @param page page번호
+ * @returns 아래 API를 통해 받아온 데이터를 10개씩 return한다.
+ * @description 데이터를 fetch하는 함수
+ * 
+ */
+
+
+const fetchData = async (page:number) => {
+	const response = await fetch(
+    `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=new+york+times&page=${page}&sort=newest&pub_date&api-key=h6IwBnNIMxY1EfOVVAeeN0XAFFfKSfQM`
+  );
+	const data = await response.json()
+	return data.response.docs;
 }
 
 export const useInfinityScrollApiQuery = () => {
-	const { data: infiniteScrollData, fetchNextPage } = useInfiniteQuery({
-		//useQuery와 사용법은 같은데, useApiQuery()에서 값을 사용 할 수는 없을까..?
-		//fetchNextPage -> 새로운 페이지의 데이터를 가져오기 위해 사용되는 함수
+	return useInfiniteQuery({
 		queryKey: ["scrolls"],
-		queryFn: async ({ pageParam = 0 }) => {
-			return await fetch(
-				`https://api.nytimes.com/svc/search/v2/articlesearch.json?
-			q=glocations:(undefined)page=${pageParam}
-			&sort=newest
-			&api-key=r4UFJLWEvtpcSMTJy0ludn1QqafnSyhL`
-			).then((res) => res.json());
-		},
+		queryFn: ({ pageParam = 1 }) => fetchData(pageParam),
+		initialPageParam: 1,
 		getNextPageParam: (lastPage, allPages) => {
-			if (allPages.length <= 2) {
-				return allPages.length;
+			const limit = 10;
+
+			if(lastPage?.length === limit) {
+				return allPages.length + 1;
 			}
-			console.log(allPages.length);
-			return allPages.length + 1;
+			return undefined;
 		},
-		initialPageParam: 0,
+	})
+		//useQuery와 사용법은 같은데, useApiQuery()에서 값을 사용 할 수는 없을까..? -> 구조분해 할당으로 변수명을 다르게 함.
+		//getNextPageParam()
+		//ㄴ 이 함수는 이전 페이지에서 다음 페이지로 이동하기 위한 매개변수를 반환하는 역할을 한다.
+		//페이징된 데이터에서 각 페이지가 어떻게 구성되었는지에 따라 함수 반환 값이 달라짐.
+		//getNextPageParm 함수를 통해 얻은 값은 fetchnextPage 함수에 전달된다.
 		//initialPageParam 사용할 페이지 번호를 설정하는 데 사용되는 프로퍼티.
 		//initialPageParam 초기 페이지를 설정해주고, 값을 설정을 해주지 않으면 undefined가 사용되어
 		//initialPageParam 첫 번째 호출 시에 pageParam이 undefined로 전달된다.
-	});
-	return { data: infiniteScrollData, fetchNextPage }
 };
